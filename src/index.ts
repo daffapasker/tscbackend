b590ea242fd6e247235631e89c426eb1ce49b5cb
Putraokta <oputra830@gmail.com>
2026-06-15 17:29:08 +0700

    sd

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function init() {
 
     app.use(
       cors({
-        origin: "*",
+        origin: "https://tsc-frontend.vercel.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
